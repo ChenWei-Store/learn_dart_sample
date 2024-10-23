0f1dad96487b0bf83095e7fd8560225fb590388c
chenwei <chenweicoder@gmail.com>
2024-10-23 22:46:22 +0800

    测试vscode提交

--- test/learn_dart_sample.dart
+++ test/learn_dart_sample.dart
@@ -2,14 +2,15 @@
  * @Author: cc
  * @Date: 2024-07-24 23:26:16
  * @LastEditors: cc
- * @LastEditTime: 2024-10-23 22:11:16
+ * @LastEditTime: 2024-10-23 22:45:02
  * @Description: 请填写简介
  */
 import 'package:learn_dart_sample/first_sample.dart';
 import 'package:test/test.dart';
 
 void main() {
-  calculate();
+  // calculate();
+
   test('calculate', () {
     expect(calculate(), 42);
   });
